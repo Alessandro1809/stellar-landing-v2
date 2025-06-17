0642320f1ccf72877a0ae5998b84d9d73e2c7daa
Alessandro1809 <dalessandro18dg@gmail.com>
2025-06-17 13:29:27 -0600

    agregando bcr proyect

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -2,9 +2,11 @@ import type { ImageMetadata } from "astro";
 import Chepena from "@/assets/proyects/Chepena.webp";
 import Zarpe from "@/assets/proyects/Zarpe.webp";
 import Erling from "@/assets/proyects/erlingnails.webp";
+import BCR from "@/assets/proyects/Bcr.webp";
 
 export interface Project {
     title: string;
+    href: string;
     image: ImageMetadata;
     imageAlt: string;
     imageWidth?: string;
@@ -15,14 +17,16 @@ export interface Project {
 export const projects: Project[] = [
     {
         title: "Chepena Restaurant",
+        href: "#",
         image: Chepena,
         imageAlt: "Chepena Restaurant Project",
         imageWidth: "74%",
-        tags: ["Website", "Frontend", "Branding"],
+        tags: ["Branding"],
         isComingSoon: false
     },
     {
         title: "Zarpe Landing Page",
+        href: "https://zarpe.netlify.app/",
         image: Zarpe,
         imageAlt: "Zarpe Landing Page Project",
         imageWidth: "80%",
@@ -31,10 +35,20 @@ export const projects: Project[] = [
     },
     {
         title: "Erling Nails",
+        href: "https://www.erlingnails.com/",
         image: Erling,
         imageAlt: "Erling Nails Project",
         imageWidth: "70%",
         tags: ["Landing Page", "Web Design", "Development"],
+        isComingSoon: false
+    },
+    {
+        title: "BCR",
+        href: "#",
+        image: BCR,
+        imageAlt: "BCR Project",
+        imageWidth: "70%",
+        tags: ["Web Design", "Development"],
         isComingSoon: false
     }
 ]; 
